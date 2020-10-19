9c0e0c2cd2f67defdda8df350a8819d4f821a381
Sha Liang <liangshaahu@gmail.com>
2020-10-19 13:04:58 +0100

    Initial Prototype2
    See the catachiing balls' dog in Challenge 2

---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -35.0f;
+    private float spawnLimitXRight = 10.0f;
+    private float spawnPosY = 22.0f;
+
+    private float startDelay = 1.0f;
+    private float spawnMaxInterval = 4.0f;
+
+    private int ballIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(0,spawnMaxInterval));
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        ballIndex = Random.Range(0,ballPrefabs.Length);
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0.2f);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+
+}
---
+++ Assets/Scripts/DestroyOutBounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutBounds : MonoBehaviour
+{
+    private float topbound = 35.0f;
+    private float lowbound = -15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topbound)
+        {
+            Destroy(gameObject);
+        }
+
+        else if (transform.position.z < lowbound)
+        {
+            Debug.Log("Game Over");
+            Destroy(gameObject);
+        }
+    }
+}
